21bc9bf00a4b7993217b40b389a5f6b8dad72468
binal845gems <binal845gems@outlook.com>
2025-08-19 15:25:58 +0530

    Create Receipt.js

---
+++ src/components/Receipt.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Receipt.css';
+
+function Receipt() {
+  return (
+    <div className="receipt">
+      <h3>Sample Receipt</h3>
+      <p>Trip: Lake Michigan Exploration</p>
+      <p>Total: $225</p>
+      <p>Date: 2025-08-19</p>
+    </div>
+  );
+}
+
+export default Receipt;
